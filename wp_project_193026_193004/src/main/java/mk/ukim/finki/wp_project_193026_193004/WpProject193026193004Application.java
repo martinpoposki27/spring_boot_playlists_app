d2cda500f263843614d284719163edbb75dc7f96
martinpoposki27 <poposkimar@gmail.com>
2022-03-28 00:19:08 +0200

    Initial commit

---
+++ wp_project_193026_193004/src/main/java/mk/ukim/finki/wp_project_193026_193004/WpProject193026193004Application.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp_project_193026_193004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpProject193026193004Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WpProject193026193004Application.class, args);
+    }
+
+}
---
+++ wp_project_193026_193004/src/test/java/mk/ukim/finki/wp_project_193026_193004/WpProject193026193004ApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp_project_193026_193004;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpProject193026193004ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
